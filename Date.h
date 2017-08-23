ea092af172281c13d7d4d63aca0d95a6667879eb
Dimas <gordeev.dmitriy@physics.msu.ru>
2017-08-23 17:53:26 +0300

    Date class

---
+++ Date.h
@@ -0,0 +1,56 @@
+#ifndef GAMELOGPARSER_DATE_H
+#define GAMELOGPARSER_DATE_H
+
+#include <ctime>
+
+struct Date
+{
+    Date() {}
+    Date(time_t ts) {
+        convert(ts);
+    }
+
+    int year;
+    int month;
+    int day;
+
+    int hour;
+    int min;
+    int sec;
+
+    void convert(time_t timestamp) {
+        struct tm * date = localtime( & timestamp );
+
+        year = date->tm_year + 1900;
+        month = date->tm_mon + 1;
+        day = date->tm_mday;
+
+        hour = date->tm_hour - 3;
+        min = date->tm_min;
+        sec = date->tm_sec;
+    }
+
+    time_t timestamp() const {
+        struct tm * date = new tm();
+
+        date->tm_year = year - 1900;
+        date->tm_mon = month - 1;
+        date->tm_mday = day;
+
+        date->tm_hour = hour + 3;
+        date->tm_min = min;
+        date->tm_sec = sec;
+
+        return mktime(date);
+    }
+
+    friend bool operator < (const Date& a, const Date& b);
+
+};
+
+bool operator < (const Date& a, const Date& b) {
+    return a.timestamp() < b.timestamp();
+}
+
+
+#endif //GAMELOGPARSER_DATE_H
--- main.cpp
+++ main.cpp
@@ -1,61 +1,10 @@
 #include <iostream>
-#include <chrono>
 #include <map>
 #include <vector>
-#include <ctime>
 #include "user-action.h"
+#include "Date.h"
 
 using namespace std;
-
-struct Date
-{
-    Date() {}
-    Date(time_t ts) {
-        convert(ts);
-    }
-
-    int year;
-    int month;
-    int day;
-
-    int hour;
-    int min;
-    int sec;
-
-    void convert(time_t timestamp) {
-        struct tm * date = localtime( & timestamp );
-
-        year = date->tm_year + 1900;
-        month = date->tm_mon + 1;
-        day = date->tm_mday;
-
-        hour = date->tm_hour - 3;
-        min = date->tm_min;
-        sec = date->tm_sec;
-    }
-
-    time_t timestamp() const {
-        struct tm * date = new tm();
-
-        date->tm_year = year - 1900;
-        date->tm_mon = month - 1;
-        date->tm_mday = day;
-
-        date->tm_hour = hour + 3;
-        date->tm_min = min;
-        date->tm_sec = sec;
-
-        return mktime(date);
-    }
-
-    friend bool operator < (const Date& a, const Date& b);
-
-};
-
-bool operator < (const Date& a, const Date& b) {
-    return a.timestamp() < b.timestamp();
-}
-
 
 map<Date, int> sort_user_actions(vector<UserAction> vua) {
 
@@ -83,6 +32,8 @@ int main() {
 
     vua.emplace_back(1483462800); // Tuesday, January 3, 2017 5:00:00 PM
     vua.emplace_back(1483466400); // Tuesday, January 3, 2017 6:00:00 PM
+
+    auto info = sort_user_actions(vua);
 
 
     return 0;
