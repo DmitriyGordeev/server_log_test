bc1a00739dc4e04667f58a3d72aefa53805d377e
Dimas <gordeev.dmitriy@physics.msu.ru>
2017-08-24 20:53:38 +0300

    load to single vector

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <algorithm>
 #include <map>
 #include <set>
 #include <vector>
 #include <dirent.h>
+#include <sys/stat.h>
 
 #include "Agr.h"
 #include "rapidjson/document.h"
@@ -157,7 +159,53 @@ bool list_files(const string& path, vector<string>& files) {
 }
 
 
-int main(int argc, char** argv) {
+// look into dir, load log and parse to single vector<UserAction>
+vector<UserAction> load_logs(const string& path, size_t logs_num = 0)
+{
+    vector<UserAction> out;
+    vector<string> file_names;
+    if(list_files(path, file_names)) {
+
+        file_names.erase(remove(file_names.begin(), file_names.end(), "."), file_names.end());
+        file_names.erase(remove(file_names.begin(), file_names.end(), ".."), file_names.end());
+
+
+        /* If files count less than specified logs_num it must be handled.
+         * By default (log_num = 0) take all found log files: */
+        size_t log_count = std::min(file_names.size(), logs_num);
+        if(logs_num == 0) {
+            log_count = file_names.size();
+        }
+
+
+        for(int i = 0; i < log_count; i++)
+        {
+            /* if suddenly trying to read dir as file, it will be skipped: */
+            struct stat buf;
+            stat((path + file_names[i]).c_str(), &buf);
+            if(S_ISDIR(buf.st_mode)) {
+                continue;
+            }
+
+            string json;
+            if(!fileio::readfile(path + file_names[i], json)) {
+                continue;
+            }
+
+            vector<UserAction> file_actions;
+            if(!parse_sample(json, file_actions)) {
+                continue;
+            }
+
+            out.insert(out.end(), file_actions.begin(), file_actions.end());
+        }
+    }
+
+    return out;
+}
+
+
+int main_(int argc, char** argv) {
 
     // read and parse input json with player actions:
     string sample_json;
@@ -178,6 +226,13 @@ int main(int argc, char** argv) {
     fileio::writefile("output_example.json", json);
 
 
+    return 0;
+}
+
+int main()
+{
+    vector<string> files;
+    vector<UserAction> actions = load_logs("../log-dir/");
 
     return 0;
 }
