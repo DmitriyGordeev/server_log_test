3ff79944ec90b4241285c6dbb7466b00a08ec606
Dimas <gordeev.dmitriy@physics.msu.ru>
2017-08-29 02:32:26 +0300

    removed unused items

--- LogLoader.h
+++ LogLoader.h
@@ -1,6 +1,6 @@
 /*
- * Class for multithreaded load and parse
- * player actions jsons from given directory
+ * Class for multithreaded load and parse player actions jsons
+ * from given directory
  */
 
 #ifndef GAMELOGPARSER_LOGLOADER_H
@@ -16,9 +16,6 @@ class LogLoader
 {
 public:
     static std::vector<UserAction> load(const std::string& log_dir, size_t num_log_files = 0, size_t num_threads = 1);
-    static bool parse_sample(const std::string& json, std::vector<UserAction>& actions);
-
-private:
     static bool single_log(const std::string& path, std::vector<UserAction>& actions);
 };
 
